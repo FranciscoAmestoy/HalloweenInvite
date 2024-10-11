73f9a9262081035856b24dab94acabe146efcb2a
Fran Amestoy <flyc0de92@gmail.com>
2024-10-11 12:42:41 -0300

    Updated characters

--- Scripts/character_selection.gd
+++ Scripts/character_selection.gd
@@ -7,10 +7,5 @@ func _on_yan_pressed() -> void:
 
 
 func _on_lore_pressed() -> void:
-	Global.character = "Lore"
-	get_tree().change_scene_to_file("res://Scenes/world.tscn") # Replace with function body.
-
-
-func _on_agus_pressed() -> void:
-	Global.character = "Agus"
+	Global.character = "Lore_&_Agus"
 	get_tree().change_scene_to_file("res://Scenes/world.tscn") # Replace with function body.

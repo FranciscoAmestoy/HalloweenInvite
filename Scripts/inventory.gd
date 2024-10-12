91efa31b90dacd687f7202b8c231f04c086bcdc3
Fran Amestoy <flyc0de92@gmail.com>
2024-10-12 15:12:31 -0300

    Added more characters

--- Scripts/inventory.gd
+++ Scripts/inventory.gd
@@ -2,23 +2,34 @@
 extends CanvasLayer
 
 var slots = [null, null, null, null]
+var select_sound = preload("res://Assets/SFX/charSelect.mp3")
+var sfx_player = null
 @onready var slot_images = [$Control/Slot1/Slot1Image, $Control/Slot2/Slot2Image, $Control/Slot3/Slot3Image, $Control/Slot4/Slot4Image]
 
 func _ready():
 	slots = Global.get_inventory()
 	update_inventory_UI()
+	sfx_player = $SFXPlayer
 
 
 func _input(event):
 		if event.is_action_pressed("Slot_1"):
 			use_item(0)
+			sfx_player.stream = select_sound
+			sfx_player.play()
 			print("Item 1 Used")
 		elif event.is_action_pressed("Slot_2"):
 			use_item(1)
+			sfx_player.stream = select_sound
+			sfx_player.play()
 		elif event.is_action_pressed("Slot_3"):
 			use_item(2)
+			sfx_player.stream = select_sound
+			sfx_player.play()
 		elif event.is_action_pressed("Slot_4"):
 			use_item(3)
+			sfx_player.stream = select_sound
+			sfx_player.play()
 
 func use_item(slot_index):
 	if slots[slot_index]:
